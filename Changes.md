26e2c8967df11e9b17340dfefebde6537bf3c3b8
Sanko Robinson <sanko@cpan.org>
2026-03-11 17:17:33 -0400

    Updating metadata...

--- Changes.md
+++ Changes.md
@@ -5,6 +5,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+
+### Changed
+- Set somewhat sane limits to prevent DoS from malformed incoming data (for example, stuffed data from Bluesky's firehose that I still can't figure out)
+
 ## [v0.0.1] - 2026-03-11
 
 ### Changed
--- lib/Codec/CBOR.pm
+++ lib/Codec/CBOR.pm
@@ -8,7 +8,7 @@ package    # Simple boolean wrapper
     sub new ( $class, $val ) { my $v = $val ? 1 : 0; bless \$v, $class }
 }
 #
-class Codec::CBOR v0.0.1 {
+class Codec::CBOR v1.0.1 {
     field %class_handlers;
     field %tag_handlers = (
         42 => sub ($data) {    # Default Tag 42 handler (generic)
@@ -170,9 +170,9 @@ class Codec::CBOR v0.0.1 {
             return Codec::CBOR::Boolean->new(0) if $info == 20;
             return Codec::CBOR::Boolean->new(1) if $info == 21;
             return undef                        if $info == 22;
-            if ( $info == 25 ) { read( $fh, my $b, 2 ); return unpack( "f>", $b ); }
-            if ( $info == 26 ) { read( $fh, my $b, 4 ); return unpack( "f>", $b ); }
-            if ( $info == 27 ) { read( $fh, my $b, 8 ); return unpack( "d>", $b ); }
+            if ( $info == 25 ) { read( $fh, my $b, 2 ); return unpack( 'f>', $b ); }
+            if ( $info == 26 ) { read( $fh, my $b, 4 ); return unpack( 'f>', $b ); }
+            if ( $info == 27 ) { read( $fh, my $b, 8 ); return unpack( 'd>', $b ); }
             return $self->_decode_value( $info, $fh );
         }
         die 'Codec::CBOR: Unsupported major type ' . $major;
@@ -180,10 +180,10 @@ class Codec::CBOR v0.0.1 {
 
     method _decode_value ( $info, $fh ) {
         return $info if $info < 24;
-        if ( $info == 24 ) { read( $fh, my $b, 1 ); return unpack( "C",  $b ); }
-        if ( $info == 25 ) { read( $fh, my $b, 2 ); return unpack( "n",  $b ); }
-        if ( $info == 26 ) { read( $fh, my $b, 4 ); return unpack( "N",  $b ); }
-        if ( $info == 27 ) { read( $fh, my $b, 8 ); return unpack( "Q>", $b ); }
+        if ( $info == 24 ) { read( $fh, my $b, 1 ); return unpack( 'C',  $b ); }
+        if ( $info == 25 ) { read( $fh, my $b, 2 ); return unpack( 'n',  $b ); }
+        if ( $info == 26 ) { read( $fh, my $b, 4 ); return unpack( 'N',  $b ); }
+        if ( $info == 27 ) { read( $fh, my $b, 8 ); return unpack( 'Q>', $b ); }
         die 'Codec::CBOR: Indefinite length or invalid info ' . $info;
     }
 };
